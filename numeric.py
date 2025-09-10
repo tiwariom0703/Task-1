188fe2faf8653d442d13eff29d8813517d8608b8
tiwariom0703 <tiwariom0703@gmail.com>
2025-09-10 11:19:55 +0530

    Add files via upload

---
+++ numeric.py
@@ -0,0 +1,13 @@
+a=float(input("Enter a first number:"))
+b=float(input("Enter a second number:"))
+c=a+b
+print('addition:',c)
+d=a-b
+print('subtraction:',d)
+e=a*b
+print('multiplication:',e)
+if b != 0:
+    f = a / b
+    print("Division:", f)
+else:
+    print("Division: Cannot divide by zero")
